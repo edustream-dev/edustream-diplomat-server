da59aa96c7b7d79101b9820631be1472bd1de204
Joe Egan <joeegan2202@gmail.com>
2020-08-23 15:12:22 -0400

    Fixed port forwarding

--- docker-run.sh
+++ docker-run.sh
@@ -1,6 +1,6 @@
 apt update -y
 apt install -y docker.io
 docker build -t edustream-diplomat-server .
-docker run --publish 433:433 --detach -it \
+docker run --publish 443:443 --detach -it \
     --restart always \
     --name run-edustream-diplomat-server edustream-diplomat-server
