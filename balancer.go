ba8ff68cf882c1331f30f101bc01be30615639ef
Joe Egan <joeegan2202@gmail.com>
2020-08-23 22:35:57 -0400

    Added logging for cid with ingest server

--- balancer.go
+++ balancer.go
@@ -89,9 +89,9 @@ type OtherServer struct{}
 func (b *IngestServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Access-Control-Allow-Origin", "*")
 
-	logger.Print("We are in ingest server!")
-
 	cid := r.URL.EscapedPath()[strings.Index(r.URL.EscapedPath(), "/"):strings.LastIndex(r.URL.EscapedPath(), "/")]
+
+	logger.Printf("We are in ingest server with cid %s!", cid)
 
 	refs := make([]struct {
 		count int
